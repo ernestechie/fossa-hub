f282830d14d411c5d7f3154554416468daefb7cc
ebitari dazi <daziebitari909@gmail.com>
2023-08-11 13:23:24 +0100

    seen changes

--- app/dashboard/events/new/page.tsx
+++ app/dashboard/events/new/page.tsx
@@ -1,4 +1,4 @@
-import EventFormComponent from '@/app/components/Events/EventForm/EventForm';
+import EventFormComponent from '@/app/components/Events/EventDetails/EventForm/EventForm';
 
 export default function NewEvent() {
   return <EventFormComponent />;
